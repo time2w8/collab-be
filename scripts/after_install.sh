436e34e4fc1f957d1eda12020950802e5c40b008
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-04-10 16:42:28 -0500

    Test pipelin 14

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #Stopping existing node servers
-echo "NPM INSTALLLLLL"
+echo "NPM INSTAL FINALLYYYYYY"
 #mkdir /home/ubuntu/projects/collab-be
 cd /home/ubuntu/projects/collab-be
 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,5 @@
-
 #!/bin/bash
-
+set -e
 # Stop all servers and start the server as a daemon
 echo "STARTING APP"
 forever stopall
