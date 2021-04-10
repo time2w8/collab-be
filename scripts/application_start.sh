9c1bbed59eaa0445f819cdb74e326d06b10bc73a
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-04-10 16:47:32 -0500

    Final Test Pipeline

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@ set -e
 # Stop all servers and start the server as a daemon
 echo "STARTING APP"
 sudo forever stopall
-sudo forever start /home/ubuntu/projects/collab-be/app.js
+sudo forever start /home/ubuntu/projects/collab-be/index.js
